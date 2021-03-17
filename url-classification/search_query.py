e98b8e139a185d5b15033094e74f88034b75bc42
bdjulian <62364747+bdjulian@users.noreply.github.com>
2021-03-17 12:29:33 -0600

    updating readme and functions

--- url-classification/search_query.py
+++ url-classification/search_query.py
@@ -1,6 +1,6 @@
+import requests
+import json
 def search(date_from, date_to, verdict):
-    import requests
-    import json
     url = "https://www.hybrid-analysis.com/api/v2/search/terms?_timestamp=1614630850155"
     headers = {'Content-Type': 'application/x-www-form-urlencoded', 'api-key': 'zcxpuuhp272a1e1bvcq18bg46bd2d94609x2l9xv1eef8812c5pu6hmi9a438676', 'user-agent': 'Falcon Sandbox', 'accept': 'application/json'}
     payload = {
--- url-classification/search_write_to_file.py
+++ url-classification/search_write_to_file.py
@@ -1,9 +1,9 @@
+import json
+from search_query import search
+import time
 def search_and_write(dates_from, dates_to):
-    import json
-    from search_query import search
-    import time
     for i in range(len(dates_from)):
         data = search(dates_from[i], dates_to[i], 5)
         with open(f'data/data{i}.txt', 'w') as outfile:
             json.dump(data, outfile)
-        time.sleep(5)
+        time.sleep(2)
