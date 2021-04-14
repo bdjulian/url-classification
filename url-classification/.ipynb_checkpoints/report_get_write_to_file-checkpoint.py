b6d4c50820b456026e6fdd910fba4a3e8e088d70
bdjulian <62364747+bdjulian@users.noreply.github.com>
2021-04-13 22:20:38 -0600

    removing cachefiles

--- url-classification/.ipynb_checkpoints/report_get_write_to_file-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import json
-import time
-from report_get import report
-def report_write(job_list):
-    for i in job_list:
-        rep = report(i)
-        with open(f'reports/reports{i}.txt', 'w') as outfile:
-            json.dump(rep, outfile)
-        time.sleep(5)
